783110a12e92d939f5e6a3e4c1b3b9a9d3f143ff
OskarLovsveden <oskarlovsveden@hotmail.com>
2021-10-14 09:33:48 +0200

    dfs implemented

--- src/ol222hf_assign3/ol222hf/MyDFS.java
+++ src/ol222hf_assign3/ol222hf/MyDFS.java
@@ -1,5 +1,7 @@
 package ol222hf_assign3.ol222hf;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import ol222hf_assign3.graphs.DFS;
@@ -8,16 +10,57 @@ import ol222hf_assign3.graphs.Node;
 
 public class MyDFS<E> implements DFS<E> {
 
+    private void innerDFS (List<Node<E>> visited, Node<E> node) {
+        // mark n as visited
+        visited.add(node);
+        node.num = visited.size();
+
+        // for each s ∈ succOf(n) do
+        Iterator<Node<E>> succs = node.succsOf();
+        while (succs.hasNext()) {
+            Node<E> succ = succs.next();
+
+            // if s not visited then
+            if (!visited.contains(succ)) {
+                // dfs(s)
+                innerDFS(visited, succ);
+            }
+        }
+        
+    }
+    
     @Override
     public List<Node<E>> dfs(DirectedGraph<E> graph, Node<E> root) {
-        // TODO Auto-generated method stub
-        return null;
+        List<Node<E>> visited = new ArrayList<Node<E>>();
+        
+        if (graph.containsNodeFor(root.item())) {
+            innerDFS(visited, root);
+        }
+        
+        return visited;
     }
-
+    
     @Override
     public List<Node<E>> dfs(DirectedGraph<E> graph) {
-        // TODO Auto-generated method stub
-        return null;
+        List<Node<E>> visited = new ArrayList<Node<E>>();
+        
+        // for each h ∈ heads(n) do
+        Iterator<Node<E>> heads = graph.heads();
+        while (heads.hasNext()) {
+            Node<E> head = heads.next();
+            
+            // if s not visited then
+            if (!visited.contains(head)) {
+                // dfs(s)
+                innerDFS(visited, head);
+            }
+        }
+
+        return visited;
+    }
+
+    private void innerPostOrder() {
+        // make nice
     }
 
     @Override
