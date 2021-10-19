3bd598d1c95d1c137fc3f7d276626ba61365d4a2
OskarLovsveden <oskarlovsveden@hotmail.com>
2021-10-19 20:23:16 +0200

    MyConnectedComponent implemented

--- src/ol222hf_assign3/ol222hf/MyConnectedComponents.java
+++ src/ol222hf_assign3/ol222hf/MyConnectedComponents.java
@@ -2,6 +2,7 @@ package ol222hf_assign3.ol222hf;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -12,42 +13,41 @@ import ol222hf_assign3.graphs.DirectedGraph;
 import ol222hf_assign3.graphs.Node;
 
 public class MyConnectedComponents<E> implements ConnectedComponents<E> {
+    private final MyDFS<E> MY_DFS = new MyDFS<E>();
 
     @Override
     public Collection<Collection<Node<E>>> computeComponents(DirectedGraph<E> dg) {
-        List<Collection<Node<E>>> connectedComponents = new ArrayList<Collection<Node<E>>>();
-
+        List<Collection<Node<E>>> cc = new ArrayList<Collection<Node<E>>>();
+        List<Collection<Node<E>>> allDFS = new ArrayList<Collection<Node<E>>>();
+        Set<Node<E>> visited = new HashSet<Node<E>>();
+        
         for (Node<E> node : dg) {
-            Set<Node<E>> cc = new HashSet<Node<E>>();
-            cc.add(node);
-            connectedComponents.add(cc);
-        }
-
-        for (int i = 0; i < connectedComponents.size(); i++) {
-            if (i+1 == connectedComponents.size()) break;
-
-            Collection<Node<E>> first = connectedComponents.get(i);
-            Collection<Node<E>> second = connectedComponents.get(i+1);
-
-            for (Node<E> node : first) {
-                
-                Iterator<Node<E>> succs = node.succsOf();
-                while (succs.hasNext()) {
-
-                    Node<E> succ = succs.next();
-                    
-                    if(second.contains(succ)) {
-                        Set<Node<E>> newCC = new HashSet<Node<E>>();
-                        newCC.addAll(first);
-                        newCC.addAll(second);
-                        connectedComponents.remove(first);
-                        connectedComponents.remove(second);
-                        connectedComponents.add(newCC);
-                    }
-                }
+            List<Node<E>> dfs = MY_DFS.dfs(dg, node);
+            if (!visited.contains(node)) {
+                visited.addAll(dfs);
+                allDFS.add(dfs);
             }
         }
 
-        return connectedComponents;
+        while(allDFS.size() != 0) {
+            Set<Node<E>> newCC = new HashSet<Node<E>>();
+            Iterator<Collection<Node<E>>> dfsItr = allDFS.iterator();
+            
+            while (dfsItr.hasNext()) {
+                Collection<Node<E>> next = dfsItr.next();
+
+                if (newCC.isEmpty()) {
+                    newCC.addAll(next);
+                    dfsItr.remove();
+                } else if (!Collections.disjoint(newCC, next)) {
+                    newCC.addAll(next);
+                    dfsItr.remove();
+                }
+            }
+
+            cc.add(newCC);
+        }
+
+        return cc;
     }
 }
